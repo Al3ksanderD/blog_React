1ca591b8276f305896731185a699930702c594e8
adudzinski <olo411271@gmail.com>
2023-03-31 15:44:03 +0200

    .

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,27 @@
-const Home = () => {
+import {useState, useEffect} from 'react'
+import BlogList from './BlogList';
 
-const handleClick = () => {
-    console.log('it works, trust me!')
-}
-const handleClickAgain = (name) => {
-    console.log('hello' + name);
-}
+const Home = () => {
+    const handleDelete = (id) => {
+        const newBLogs = blogs.filter(blog => blog.id != id);
+        setBlogs(newBLogs);
+    }
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
+
+      useEffect(() => {
+        console.log('use effect run');
+        console.log(blogs);
+      })
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me!</button>
-            <button onClick={() => handleClickAgain('mario')} >Click me again!</button>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" />
         </div>
      );
 }
